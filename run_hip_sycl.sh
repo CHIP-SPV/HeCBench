5d200e2e06ca68d3482f85318962cc385a37be13
Paulius Velesko <pvelesko@pglc.io>
2024-09-30 18:42:44 +0300

    WIP

--- run_hip_sycl.sh
+++ run_hip_sycl.sh
@@ -18,8 +18,9 @@ export CHIP_DEVICE_TYPE=gpu
 export CHIP_LOGLEVEL=crit
 
 # -------------------
-
+source /etc/profile.d/modules.sh
 module load oneapi/2024.1.0
+module load HIP/chipStar/v1.2
 
 # ╭─pvelesko@cupcake /space/pvelesko/HeCBench ‹chipStar-bench●› 
 # ╰─$ clinfo -l                                                                                                                                                                                                             130 ↵
@@ -34,13 +35,13 @@ if [ $OPENCL -ne 0 ]; then
   export SYCL_CACHE_PERSISTENT=0
   export ONEAPI_DEVICE_SELECTOR="opencl:gpu"
   
-  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o test_FULL_${REPEATS}_x_sycl_strict_oclBE.csv --sycl-type opencl sycl
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o chipStar-v1.2-test_FULL_${REPEATS}_x_sycl_strict_oclBE.csv --sycl-type opencl sycl
 fi
 
 if [ $LEVEL0 -ne 0 ]; then
   export SYCL_CACHE_PERSISTENT=0
   export ONEAPI_DEVICE_SELECTOR="level0:gpu"
-  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o test_FULL_${REPEATS}_x_sycl_strict_l0BE.csv --sycl-type opencl sycl
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o chipStar-v1.2-test_FULL_${REPEATS}_x_sycl_strict_l0BE.csv --sycl-type opencl sycl
 fi
 
 
@@ -51,7 +52,7 @@ if [ $OPENCL -ne 0 ]; then
   # export CHIP_BE=opencl
   # export CHIP_PLATFORM=1
   module load opencl/dgpu
-  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o test_FULL_${REPEATS}_x_hip_strict_oclBE.csv hip
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o chipStar-v1.2-test_FULL_${REPEATS}_x_hip_strict_oclBE.csv hip
   module unload opencl/dgpu
 fi
 
@@ -60,9 +61,48 @@ fi
 if [ $LEVEL0 -ne 0 ]; then
 
   module load level-zero/dgpu
-  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o test_FULL_${REPEATS}_x_hip_strict_l0BE.csv hip
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o chipStar-v1.2-test_FULL_${REPEATS}_x_hip_strict_l0BE.csv hip
   module unload level-zero/dgpu
 fi
 
 #############################################
 
+
+module unload HIP/chipStar/v1.2
+module load HIP/chipStar/v1.1
+
+if [ $OPENCL -ne 0 ]; then
+  export SYCL_CACHE_PERSISTENT=0
+  export ONEAPI_DEVICE_SELECTOR="opencl:gpu"
+  
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o chipStar-v1.1-test_FULL_${REPEATS}_x_sycl_strict_oclBE.csv --sycl-type opencl sycl
+fi
+
+if [ $LEVEL0 -ne 0 ]; then
+  export SYCL_CACHE_PERSISTENT=0
+  export ONEAPI_DEVICE_SELECTOR="level0:gpu"
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} --extra-compile-flags="-fp-model=precise -fno-sycl-instrument-device-code" -o chipStar-v1.1-test_FULL_${REPEATS}_x_sycl_strict_l0BE.csv --sycl-type opencl sycl
+fi
+
+
+#############################################
+
+if [ $OPENCL -ne 0 ]; then
+
+  # export CHIP_BE=opencl
+  # export CHIP_PLATFORM=1
+  module load opencl/dgpu
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o chipStar-v1.1-test_FULL_${REPEATS}_x_hip_strict_oclBE.csv hip
+  module unload opencl/dgpu
+fi
+
+# -------------------
+
+if [ $LEVEL0 -ne 0 ]; then
+
+  module load level-zero/dgpu
+  ./scripts/autohecbench.py --warmup false --repeat ${REPEATS} -o chipStar-v1.1-test_FULL_${REPEATS}_x_hip_strict_l0BE.csv hip
+  module unload level-zero/dgpu
+fi
+
+#############################################
