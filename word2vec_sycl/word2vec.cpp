f22f562bbb8b33cc1c7e1a54fd0b8471931538f4
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:56 +0300

    [word2vec] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- word2vec_sycl/word2vec.cpp
+++ word2vec_sycl/word2vec.cpp
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <math.h>
 #include <pthread.h>
+#include <chrono>
 #include "cbow.h"
 
 const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
@@ -40,7 +41,7 @@ int iter = 5,  classes = 0;
 real alpha = 0.025, starting_alpha, sample = 1e-3;
 real *syn0;
 int * sen;
-clock_t start;
+auto start = std::chrono::steady_clock::now();
 
 int hs = 0, negative = 5;
 int table_size = 1e8;
@@ -51,7 +52,8 @@ void InitUnigramTable() {
   double train_words_pow = 0;
   double d1, power = 0.75;
   table = (int *)malloc(table_size * sizeof(int));
-  for (a = 0; a < vocab_size; a++) train_words_pow += pow(vocab[a].cn, power);
+  for (a = 0; a < vocab_size; a++) train_words_pow +=
+      pow(vocab[a].cn, power);
   i = 0;
   d1 = pow(vocab[i].cn, power) / train_words_pow;
   for (a = 0; a < table_size; a++) {
@@ -344,21 +346,20 @@ void InitNet() {
   CreateBinaryTree();
 }
 
-typedef struct thread_arguments {
-  long n;
-  sycl::queue *q;
-} thargs_t;
 
 void *TrainModelThread(void *id) {
-  // read the arguments
-  unsigned int next_random = ((thargs_t*)id)->n;
-  sycl::queue &q = *(((thargs_t*)id)->q);
+#ifdef USE_GPU
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
+#else
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
+#endif
+
+  unsigned int next_random = (long)id;
 
   int   word, sentence_length = 0;
   long long word_count = 0, last_word_count = 0;
   int    local_iter = iter;
   int sentence_num;
-  clock_t now;
   real * alpha_ptr = (float *) sen + MAX_SENTENCE_NUM * MAX_SENTENCE_LENGTH;
   FILE *fi = fopen(train_file, "rb");
   fseek(fi, file_size / (int)num_threads * (long)id, SEEK_SET);
@@ -370,10 +371,12 @@ void *TrainModelThread(void *id) {
       word_count_actual += word_count - last_word_count;
       last_word_count = word_count;
       if ((debug_mode > 1)) {
-        now=clock();
+        auto now = std::chrono::steady_clock::now();
+        auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(now - start).count();
+        time *= 1e-9f;
         printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
             word_count_actual / (real)(iter * train_words + 1) * 100,
-            word_count_actual / ((real)(now - start + 1) / (real)CLOCKS_PER_SEC * 1000));
+            word_count_actual / ((real) time * 1000));
         fflush(stdout);
       }
       alpha = starting_alpha * (1 - word_count_actual / (real)(iter * train_words + 1));
@@ -436,10 +439,7 @@ void TrainModel() {
   }
 
   long a, b, c, d;
-
   pthread_t *pt = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
-  thargs_t *pa = (thargs_t *) malloc (num_threads * sizeof(thargs_t));
-
   printf("Starting training using file %s\n", train_file);
   starting_alpha = alpha;
   if (read_vocab_file[0] != 0) ReadVocab(); else LearnVocabFromTrainFile();
@@ -454,17 +454,13 @@ void TrainModel() {
 #else
   sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-    
+
   initializeGPU(q);
 
-  start = clock();
+  start = std::chrono::steady_clock::now();
 
   // Training on a GPU
-  for (a = 0; a < num_threads; a++) {
-    pa[a].n = a;
-    pa[a].q = &q;
-    pthread_create(&pt[a], NULL, TrainModelThread, &pa[a]);
-  }
+  for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
 
   // Training complete
   for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
@@ -525,7 +521,6 @@ void TrainModel() {
     free(cl);
   }
   free(pt);
-  free(pa);
   fclose(fo);
 }
 
