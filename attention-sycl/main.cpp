b774abb0e591b859bc141fa1c9da8f6fb74eb9f4
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:01 +0300

    [attention] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- attention-sycl/main.cpp
+++ attention-sycl/main.cpp
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <chrono>
+#include <random>
 #include <sycl/sycl.hpp>
+#include "kernels.h"
 #include "reference.h"
 
+inline int grids(int p, int b, int warpSize) {
+  int waves = b / warpSize;
+  return (p + waves - 1) / waves;
+}
+
 float* attention_device(const float* key, const float* value, const float* query,
-                        const int n, const int d, const int repeat) 
+                        const int n, const int d, const int impl_num, const int repeat)
 {
 #ifdef USE_GPU
   sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
@@ -14,124 +21,225 @@ float* attention_device(const float* key, const float* value, const float* query
   sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
 
+  auto sg_sizes = q.get_device().get_info<sycl::info::device::sub_group_sizes>();
+  auto r = std::max_element(sg_sizes.begin(), sg_sizes.end());
+  int warpSize = *r;
+
+  size_t q_size = (size_t)d;
+  size_t r_size = (size_t)n;
+  size_t kv_size = (size_t)d * n;
+  size_t r_size_bytes = r_size * sizeof(float);
+  size_t q_size_bytes = q_size * sizeof(float);
+  size_t kv_size_bytes = kv_size * sizeof(float);
+
   // input
-  float *d_key = sycl::malloc_device<float>(n*d, q);
-  q.memcpy(d_key, key, sizeof(float) * n * d);
+  float *d_key;
+  d_key = sycl::malloc_device<float>(kv_size, q);
+  q.memcpy(d_key, key, kv_size_bytes).wait();
 
-  float *d_value = sycl::malloc_device<float>(n*d, q);
-  q.memcpy(d_value, value, sizeof(float) * n * d);
+  float *d_value;
+  d_value = sycl::malloc_device<float>(kv_size, q);
+  q.memcpy(d_value, value, kv_size_bytes).wait();
 
-  float *d_query = sycl::malloc_device<float>(d, q);
-  q.memcpy(d_query, query, sizeof(float) * d);
+  float *d_query;
+  d_query = sycl::malloc_device<float>(d, q);
+  q.memcpy(d_query, query, q_size_bytes).wait();
 
   // intermediate
-  float *d_dot_product = sycl::malloc_device<float>(n, q);
-  float *d_score = sycl::malloc_device<float>(n, q);
-  float *d_exp_sum = sycl::malloc_device<float>(1, q);
+  float *d_dot_product;
+  d_dot_product = sycl::malloc_device<float>(n, q);
+
+  float *d_exp_sum;
+  d_exp_sum = sycl::malloc_device<float>(1, q);
 
   // result
-  float *output = (float*) malloc (d * sizeof(float));
-  float *d_output = sycl::malloc_device<float>(d, q);
-
-  sycl::range<1> n_gws ((n+255)/256*256);
-  sycl::range<1> n_lws (256);
-  sycl::range<1> d_gws ((d+255)/256*256);
-  sycl::range<1> d_lws (256);
+  float *output = (float*) malloc (q_size_bytes);
+  float *d_output;
+  d_output = sycl::malloc_device<float>(d, q);
 
   q.wait();
-  auto start = std::chrono::steady_clock::now();
 
-  for (int k = 0; k < repeat; k++) {
-    q.memset(d_exp_sum, 0, sizeof(float));
+  if (impl_num == 3) {
 
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class k1>(sycl::nd_range<1>(n_gws, n_lws), [=] (sycl::nd_item<1> item) {
-        int i = item.get_global_id(0);
-        if (i < n) {
-          float sum = 0;
-          for (int j = 0; j < d; j++)
-            sum += d_key[i * d + j] * d_query[j];
-          d_dot_product[i] = sum;
-          auto ao = sycl::atomic_ref<float, 
-            sycl::memory_order::relaxed,
-            sycl::memory_scope::device,
-            sycl::access::address_space::global_space> (d_exp_sum[0]);
-          ao.fetch_add(sycl::exp(sum));
-        }
+    auto start = std::chrono::steady_clock::now();
+
+    for (int k = 0; k < repeat; k++) {
+      q.memset(d_exp_sum, 0, 4);
+      q.parallel_for(sycl::nd_range<1>(sycl::range<1>(grids(n, 256, warpSize) * 256),
+                                       sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel1_warpReduce(d_key, d_query, d_dot_product,
+                                              d_exp_sum, n, d, item);
+                         });
+      q.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(d * 256),
+                                           sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel2_blockReduce(d_exp_sum, d_dot_product,
+                                               d_value, d_output, n, d,
+                                               item);
+                         });
       });
-    });
+    }
 
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class k2>(sycl::nd_range<1>(n_gws, n_lws), [=] (sycl::nd_item<1> item) {
-        int i = item.get_global_id(0);
-        if (i < n)
-          d_score[i] = sycl::exp(d_dot_product[i]) / d_exp_sum[0];
-      });
-    });
-
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class k3>(sycl::nd_range<1>(d_gws, d_lws), [=] (sycl::nd_item<1> item) {
-        int j = item.get_global_id(0);
-        if (j < d) {
-          float sum = 0;
-          for (int i = 0; i < n; i++)
-            sum += d_score[i] * d_value[i * d + j];
-          d_output[j] = sum;
-        }
-      });
-    });
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
   }
 
-  q.wait();
-  auto end = std::chrono::steady_clock::now();
-  auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+  else if (impl_num == 2) {
 
-  q.memcpy(output, d_output, sizeof(float) * d).wait();
+    auto start = std::chrono::steady_clock::now();
 
-  sycl::free(d_key, q);
+    for (int k = 0; k < repeat; k++) {
+      q.memset(d_exp_sum, 0, 4);
+      q.parallel_for(sycl::nd_range<1>(sycl::range<1>(grids(n, 256, warpSize) * 256),
+                                       sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel1_warpReduce(d_key, d_query, d_dot_product,
+                                              d_exp_sum, n, d, item);
+                         });
+      q.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(grids(d, 256, warpSize) * 256),
+                                           sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel2_warpReduce(d_exp_sum, d_dot_product,
+                                               d_value, d_output, n, d,
+                                               item);
+                         });
+      });
+    }
+
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+  }
+
+  else if (impl_num == 1) {
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int k = 0; k < repeat; k++) {
+      q.memset(d_exp_sum, 0, 4);
+      q.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(n * 256),
+                                           sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel1_blockReduce(d_key, d_query, d_dot_product,
+                                               d_exp_sum, n, d, item);
+                         });
+      });
+      q.submit([&](sycl::handler &cgh) {
+        cgh.parallel_for(sycl::nd_range<1>(sycl::range<1>(d * 256),
+                                           sycl::range<1>(256)),
+                         [=](sycl::nd_item<1> item) {
+                           attention_kernel2_blockReduce(d_exp_sum, d_dot_product,
+                                               d_value, d_output, n, d,
+                                               item);
+                         });
+      });
+    }
+
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+  }
+
+  else {
+    float *d_score;
+    d_score = sycl::malloc_device<float>(n, q);
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int k = 0; k < repeat; k++) {
+      q.memset(d_exp_sum, 0, 4);
+      q.parallel_for(
+          sycl::nd_range<1>(sycl::range<1>((n + 255) / 256) *
+                            sycl::range<1>(256),
+                            sycl::range<1>(256)),
+          [=](sycl::nd_item<1> item) {
+            attention_kernel1(d_key, d_query, d_dot_product, d_exp_sum, n, d, item);
+          });
+      q.parallel_for(
+          sycl::nd_range<1>(sycl::range<1>((n + 255) / 256) *
+                            sycl::range<1>(256),
+                            sycl::range<1>(256)),
+          [=](sycl::nd_item<1> item) {
+            attention_kernel2(d_exp_sum, d_dot_product, d_score, n, item);
+          });
+      q.parallel_for(
+          sycl::nd_range<1>(sycl::range<1>((d + 255) / 256) *
+                            sycl::range<1>(256),
+                            sycl::range<1>(256)),
+          [=](sycl::nd_item<1> item) {
+            attention_kernel3(d_score, d_value, d_output, n, d, item);
+          });
+    }
+
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    printf("Average execution time of kernels %f (ms)\n", time * 1e-6f / repeat);
+    sycl::free(d_score, q);
+  }
+
+  q.memcpy(output, d_output, q_size_bytes).wait();
   sycl::free(d_value, q);
-  sycl::free(d_query, q);
-  sycl::free(d_dot_product, q);
-  sycl::free(d_score, q);
-  sycl::free(d_exp_sum, q);
   sycl::free(d_output, q);
-
+  sycl::free(d_key, q);
+  sycl::free(d_dot_product, q);
+  sycl::free(d_exp_sum, q);
   return output;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    printf("Usage: %s <rows> <columns> <repeat>\n", argv[0]);
+  if (argc != 5) {
+    printf("Usage: %s <rows> <columns> <implementation> <repeat>\n", argv[0]);
+    printf("implementation 0: naive\n");
+    printf("implementation 1: fused kernels with block reduce\n");
+    printf("implementation 2: fused kernels with warp reduce\n");
+    printf("implementation 3: fused kernels with mixed reduce\n");
     return 1;
   }
   const int n = atoi(argv[1]);
   const int d = atoi(argv[2]);
-  const int r = atoi(argv[3]);
+  const int k = atoi(argv[3]);
+  const int r = atoi(argv[4]);
 
   // input
-  float* key = (float*) malloc (n * d * sizeof(float));
-  float* value = (float*) malloc (n * d * sizeof(float));
-  float* query = (float*) malloc (d * sizeof(float));
+  size_t q_size = (size_t)d;
+  size_t kv_size = (size_t)d * n;
+  size_t q_size_bytes = q_size * sizeof(float);
+  size_t kv_size_bytes = kv_size * sizeof(float);
 
-  srand(2);
-  for (int i = 0; i < n * d; i++) {
-    key[i] = 0.1;
-    value[i] = 0.3;
-    if (rand() % 2)
-      query[i % d] = value[i] + key[i] ;
-    else
-      query[i % d] = value[i] - key[i] ;
+  float* key = (float*) malloc (kv_size_bytes);
+  float* value = (float*) malloc (kv_size_bytes);
+  float* query = (float*) malloc (q_size_bytes);
+
+  std::mt19937 gen(19937);
+  std::uniform_real_distribution<float> dist(-0.01f, 0.01f);
+
+  for (size_t i = 0; i < kv_size; i++) {
+    key[i] = dist(gen);
+    value[i] = dist(gen);
+    query[i % d] = dist(gen);
   }
 
   float* hout = attention_host(key, value, query, n, d);
+  float* dout = attention_device(key, value, query, n, d, k, r);
 
-  float* dout = attention_device(key, value, query, n, d, r);
-
-  float rmse = 0;
-  for (int i = 0; i < d; i++) 
-    rmse += (hout[i] - dout[i]) * (hout[i] - dout[i]);
-  printf("RMSE = %f\n", sqrtf(rmse / d));
+  bool ok = true;
+  for (int i = 0; i < d; i++) {
+    if (fabsf(hout[i] - dout[i]) > 1e-3f) {
+      ok = false;
+      break;
+    }
+  }
+  printf("%s\n", ok ? "PASS" : "FAIL");
+  if (!ok) exit(1);
 
   free(key);
   free(value);
