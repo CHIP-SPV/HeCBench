37c0b09d932467272cc779e418f4bc2e8999dc22
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:45 +0300

    [reverse] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- reverse-cuda/main.cu
+++ reverse-cuda/main.cu
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
   // save device result
   int test[len];
 
-  // save expected results after performing preverse operations even/odd times
+  // save expected results after performing reverse operations even/odd times
   int error = 0;
   int gold_odd[len];
   int gold_even[len];
@@ -77,6 +77,7 @@ int main(int argc, char* argv[]) {
   
   printf("Total kernel execution time: %f (s)\n", time * 1e-9f);
   printf("%s\n", error ? "FAIL" : "PASS");
+  if (error) exit(1);
 
   cudaFree(d_test);
   return 0;
