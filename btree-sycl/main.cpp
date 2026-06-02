5a16735ba8af1c860aab2acdeff2f583a47437ac
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:05 +0300

    [btree] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- btree-sycl/main.cpp
+++ btree-sycl/main.cpp
@@ -25,7 +25,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <CL/sycl.hpp>
+#include <sycl/sycl.hpp>
 
 #include <algorithm>
 #include <random>
@@ -92,11 +92,10 @@ int main(int argc, char *argv[]) {
   std::shuffle(query_keys.begin(), query_keys.end(), g);
 
 #ifdef USE_GPU
-  sycl::gpu_selector dev_sel;
+  sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
 #else
-  sycl::cpu_selector dev_sel;
+  sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
-  sycl::queue q(dev_sel);
 
   GpuBTree::GpuBTreeMap<uint32_t, uint32_t, uint32_t, PoolAllocator> btree(q);
 
@@ -162,6 +161,10 @@ int main(int argc, char *argv[]) {
   if (ok)
     printf("PASS. ([%0.2f%%] queries exist in search.)\n",
             float(exist_count) / float(numQueries) * 100.0);
+  else {
+    printf("FAIL\n");
+    exit(1);
+  }
 
   printf("Tree size: %f GiBs.\n", float(btree.compute_usage()));
 
