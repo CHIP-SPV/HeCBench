8f8ee6b03bc7286e0c99e61415c5e6f297034e54
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:22 +0300

    [heat] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- heat-omp/heat.cpp
+++ heat-omp/heat.cpp
@@ -34,7 +34,6 @@
 #include <omp.h>
 
 // Key constants used in this program
-#define PI acos(-1.0) // Pi
 #define LINE "--------------------\n" // A line for fancy output
 
 double solution(const double t, const double x, const double y, const double alpha, const double length);
@@ -119,7 +118,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < n; ++i) {
       double y = (j+1)*dx; // Physical y position
       double x = (i+1)*dx; // Physical x position
-      u[i+j*n] = sin(PI * x / length) * sin(PI * y / length);
+      u[i+j*n] = sin(acos(-1.0) * x / length) * sin(acos(-1.0) * y / length);
     }
   }
 
@@ -194,14 +193,14 @@ int main(int argc, char *argv[]) {
 // True answer given by the manufactured solution
 double solution(const double t, const double x, const double y, const double alpha, const double length) {
 
-  return exp(-2.0*alpha*PI*PI*t/(length*length)) * sin(PI*x/length) * sin(PI*y/length);
+  return exp(-2.0*alpha*M_PI*M_PI*t/(length*length)) * sin(M_PI*x/length) * sin(M_PI*y/length);
 
 }
 
 
 // Computes the L2-norm of the computed grid and the MMS known solution
 // The known solution is the same as the boundary function.
-double l2norm(const int n, const double * restrict u, const int nsteps, const double dt,
+double l2norm(const int n, const double * u, const int nsteps, const double dt,
               const double alpha, const double dx, const double length) {
 
   // Final (real) time simulated
