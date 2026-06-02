0d1189b08f45e1b8b628884f3f3cda869df26133
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:00 +0300

    [atomicAggregate] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- atomicAggregate-sycl/main.cpp
+++ atomicAggregate-sycl/main.cpp
@@ -11,10 +11,49 @@ inline int ffs(int x) {
   return (x == 0) ? 0 : sycl::ctz(x) + 1;
 }
 
+#define DPCT
+/// https://github.com/oneapi-src/SYCLomatic/
+/// The function match_any_over_sub_group conducts a comparison of values
+/// across work-items within a sub-group. match_any_over_sub_group return a mask
+/// in which some bits are set to 1, indicating that the values provided by
+/// the work-items represented by these bits are equal. The n-th bit of mask
+/// representing the work-item with id n. The parameter \p member_mask
+/// indicating the work-items participating the call.
+template <typename T>
+unsigned int match_any_over_sub_group(sycl::sub_group g, unsigned member_mask,
+                                      T value) {
+  static_assert(std::is_trivially_copyable_v<T>,
+                "Value type must be trivially copyable type.");
+  if (!member_mask) {
+    return 0;
+  }
+  unsigned int id = g.get_local_linear_id();
+  unsigned int flag = 0, result = 0, reduce_result = 0;
+  unsigned int bit_index = 0x1 << id;
+  bool is_participate = member_mask & bit_index;
+  T broadcast_value = 0;
+  bool matched = false;
+  while (flag != member_mask) {
+    broadcast_value =
+        sycl::select_from_group(g, value, sycl::ctz((~flag & member_mask)));
+    reduce_result = sycl::reduce_over_group(
+        g, is_participate ? (broadcast_value == value ? bit_index : 0) : 0,
+        sycl::plus<>());
+    flag |= reduce_result;
+    matched = reduce_result & bit_index;
+    result = matched * reduce_result + (1 - matched) * result;
+  }
+  return result;
+}
+
+
 // increment the value at ptr by 1 and return the old value
 int atomicAggInc(int* ptr, sycl::nd_item<1> &item) {
   int mask;
   auto sg = item.get_sub_group();
+#ifdef DPCT
+  mask = match_any_over_sub_group(sg, 0xFFFFFFFF, (unsigned long long)ptr);
+#else
   for (int i = 0; i < warpSize; i++) {
     unsigned long long tptr = sycl::select_from_group(sg, (unsigned long long)ptr, i);
     auto gb = sycl::ext::oneapi::group_ballot(sg, (tptr == (unsigned long long)ptr));
@@ -22,12 +61,13 @@ int atomicAggInc(int* ptr, sycl::nd_item<1> &item) {
     gb.extract_bits(my_mask, 0);
     if (i == (item.get_local_id(0) & (warpSize - 1))) mask = my_mask;
   }
+#endif
 
   int leader = ffs(mask) - 1; // select a leader
   int res = 0;
   unsigned lane_id = item.get_local_id(0) % warpSize;
   if (lane_id == leader) {                 // leader does the update
-    sycl::atomic_ref<int, 
+    sycl::atomic_ref<int,
        sycl::memory_order::relaxed,
        sycl::memory_scope::device,
        sycl::access::address_space::global_space> ao (*ptr);
@@ -37,12 +77,10 @@ int atomicAggInc(int* ptr, sycl::nd_item<1> &item) {
   return res + sycl::popcount(mask & ((1 << lane_id) - 1)); // compute old value
 }
 
-void k(int *d, sycl::nd_item<1> &item) {
-  int *ptr = d + item.get_local_id(0) % 32;
+void k(int *d, int s, sycl::nd_item<1> &item) {
+  int *ptr = d + item.get_local_id(0) % s;
   atomicAggInc(ptr, item);
 }
-
-const int ds = 32;
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -51,8 +89,8 @@ int main(int argc, char *argv[]) {
   }
   const int repeat = atoi(argv[1]);
 
-  int *d_d, *h_d;
-  h_d = new int[ds];
+  const int nBlocks = 65536;
+  const int blockSize = 256;
 
 #ifdef USE_GPU
   sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order());
@@ -60,43 +98,50 @@ int main(int argc, char *argv[]) {
   sycl::queue q(sycl::cpu_selector_v, sycl::property::queue::in_order());
 #endif
 
-  d_d = (int *)sycl::malloc_device(ds * sizeof(d_d[0]), q);
-  q.memset(d_d, 0, ds * sizeof(d_d[0]));
+  for (int ds = 32; ds >= 1; ds = ds / 2) {
 
-  q.wait();
+    int *d_d, *h_d;
+    h_d = new int[ds];
 
-  sycl::range<1> gws (256 * 32 * 256);
-  sycl::range<1> lws (256);
+    d_d = (int *)sycl::malloc_device(ds * sizeof(d_d[0]), q);
+    q.memset(d_d, 0, ds * sizeof(d_d[0]));
 
-  auto start = std::chrono::steady_clock::now();
+    q.wait();
 
-  for (int i = 0; i < repeat; i++) {
-    q.submit([&] (sycl::handler &cgh) {
-      cgh.parallel_for<class kernel>(
-        sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item) 
-                       [[sycl::reqd_sub_group_size(warpSize)]] {
-        k(d_d, item);
+    sycl::range<1> gws (blockSize * nBlocks);
+    sycl::range<1> lws (blockSize);
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < repeat; i++) {
+      q.submit([&] (sycl::handler &cgh) {
+        cgh.parallel_for<class kernel>(
+          sycl::nd_range<1>(gws, lws), [=](sycl::nd_item<1> item)
+                         [[sycl::reqd_sub_group_size(warpSize)]] {
+          k(d_d, ds, item);
+        });
       });
-    });
-  }
-
-  q.wait();
-
-  auto end = std::chrono::steady_clock::now();
-  std::chrono::duration<float> time = end - start;
-  printf("Total kernel time: %f (s)\n", time.count());
-
-  q.memcpy(h_d, d_d, ds * sizeof(d_d[0])).wait();
-
-  bool ok = true;
-  for (int i = 0; i < ds; i++) {
-    if (h_d[i] != 256 * 256 * repeat) {
-      ok = false;
-      break;
     }
+
+    q.wait();
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<float> time = end - start;
+    printf("Total kernel time (%d locations): %f (s)\n", ds, time.count());
+
+    q.memcpy(h_d, d_d, ds * sizeof(d_d[0])).wait();
+
+    bool ok = true;
+    for (int i = 0; i < ds; i++) {
+      if (h_d[i] != blockSize / ds * nBlocks * repeat) {
+        ok = false;
+        break;
+      }
+    }
+    printf("%s\n", ok ? "PASS" : "FAIL");
+  if (!ok) exit(1);
+    sycl::free(d_d, q);
+    delete [] h_d;
   }
-  printf("%s\n", ok ? "PASS" : "FAIL");
-  sycl::free(d_d, q);
-  delete [] h_d;
   return 0;
 }
