06640508413421f84898699112c0ac9240e8a6cb
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:18 +0300

    [frechet] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- frechet-sycl/norm3.h
+++ frechet-sycl/norm3.h
@@ -25,50 +25,7 @@ double norm3(int i, int j, const double *c1, const double *c2)
   return dist;
 }
 
-double recursive_norm3(int i, int j, int n_2, double *ca,
-                       const double *c1, const double *c2)
-{
-  /*
-   * Target the shortcut to the (i, j)-th entry of the matrix `ca`
-   *
-   * Once again, notice the 1-offset.
-   */
-  double *ca_ij = ca + (i - 1)*n_2 + (j - 1);
-
-  /* This implements the algorithm from [1] */
-  if (*ca_ij > -1.0)
-  {
-    return *ca_ij;
-  }
-  else if ((i == 1) && (j == 1))
-  {
-    *ca_ij = norm3(1, 1, c1, c2);
-  }
-  else if ((i > 1) && (j == 1))
-  {
-    *ca_ij = sycl::fmax(recursive_norm3(i - 1, 1, n_2, ca, c1, c2), norm3(i, 1, c1, c2));
-  }
-  else if ((i == 1) && (j > 1))
-  {
-    *ca_ij = sycl::fmax(recursive_norm3(1, j - 1, n_2, ca, c1, c2), norm3(1, j, c1, c2));
-  }
-  else if ((i > 1) && (j > 1))
-  {
-    *ca_ij = sycl::fmax(
-        sycl::fmin(sycl::fmin(
-            recursive_norm3(i - 1, j    , n_2, ca, c1, c2),
-            recursive_norm3(i - 1, j - 1, n_2, ca, c1, c2)),
-            recursive_norm3(i,     j - 1, n_2, ca, c1, c2)),
-        norm3(i, j, c1, c2));
-  }
-  else
-  {
-    *ca_ij = INFINITY;
-  }
-
-  return *ca_ij;
-}
-
+// Iterative DP fill (replaces recursive_norm3 to remove SYCL-illegal recursion).
 void distance_norm3 (
   sycl::nd_item<2> &item,
   int n_1, int n_2,
@@ -76,8 +33,22 @@ void distance_norm3 (
   const double *__restrict c1,
   const double *__restrict c2)
 {
-  int i = item.get_global_id(1);
-  int j = item.get_global_id(0);
-  if (j >= 1 && j <= n_2 && i >= 1 && i <= n_1)
-    recursive_norm3(i, j, n_2, ca, c1, c2);
+  if (item.get_global_id(0) != 0 || item.get_global_id(1) != 0) return;
+  for (int i = 1; i <= n_1; i++) {
+    for (int j = 1; j <= n_2; j++) {
+      double *ca_ij = ca + (i - 1)*n_2 + (j - 1);
+      if (i == 1 && j == 1) {
+        *ca_ij = norm3(1, 1, c1, c2);
+      } else if (i > 1 && j == 1) {
+        *ca_ij = sycl::fmax(*(ca + (i-2)*n_2 + 0), norm3(i, 1, c1, c2));
+      } else if (i == 1 && j > 1) {
+        *ca_ij = sycl::fmax(*(ca + 0*n_2 + (j-2)), norm3(1, j, c1, c2));
+      } else {
+        double a = *(ca + (i-2)*n_2 + (j-1));
+        double b = *(ca + (i-2)*n_2 + (j-2));
+        double c = *(ca + (i-1)*n_2 + (j-2));
+        *ca_ij = sycl::fmax(sycl::fmin(sycl::fmin(a, b), c), norm3(i, j, c1, c2));
+      }
+    }
+  }
 }
