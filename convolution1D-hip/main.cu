750c7c07132be5eb6065cf4afab84887fd42a2c3
Paulius Velesko <pvelesko@pglc.io>
2026-06-02 19:07:10 +0300

    [convolution1D] - port from chipStar-bench-2025: autobench launcher (LAUNCHER), binary=main, exit codes, C++14->C++17, AdaptiveCpp filter; run/subset tuning and source fixes

--- convolution1D-hip/main.cu
+++ convolution1D-hip/main.cu
@@ -124,6 +124,7 @@ void reference(const T *h_in,
     }
   }
   printf("%s\n", ok ? "PASS" : "FAIL");
+  if (!ok) exit(1);
 }
 
 template <typename T>
--- convolution1D-sycl/main.cpp
+++ convolution1D-sycl/main.cpp
@@ -146,6 +146,7 @@ void reference(const T *h_in,
     }
   }
   printf("%s\n", ok ? "PASS" : "FAIL");
+  if (!ok) exit(1);
 }
 
 template <typename T>
